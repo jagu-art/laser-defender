e72c045769fa3eac5dec186da409f4733274b95c
Juan Gremes <13813786+gremjua@users.noreply.github.com>
2023-07-19 07:10:18 +0200

    configure bounds and input system

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+    float paddingLeft;
+    float paddingRight;
+    Vector2 rawInput;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    void Start()
+    {
+        InitBounds();
+    }
+    void Update()
+    {
+        Move();
+    }
+
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0)); // bottom left corner of viewport
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1)); // top right corner of viewport
+        paddingLeft = paddingRight = gameObject.GetComponentInChildren<Renderer>().bounds.extents.x;
+    }
+
+    void Move()
+    {
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2(); // stop player from moving out of viewport
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+        Debug.Log(rawInput);
+    }
+}
