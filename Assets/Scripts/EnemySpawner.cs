3cb209cea58a6c1ce125cfe7fdebcc13f486941e
Juan Gremes <13813786+gremjua@users.noreply.github.com>
2023-07-26 07:47:02 +0200

    add infinite waves

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,6 +7,7 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<WaveConfigSO> waveConfigs;
     [SerializeField] float timeBetweenWaves = 0f;
+    [SerializeField] bool isLooping = true;
     WaveConfigSO currentWave;
     // Start is called before the first frame update
     void Start()
@@ -21,19 +22,21 @@ public class EnemySpawner : MonoBehaviour
 
     private IEnumerator SpawnEnemyWaves()
     {
-        foreach(WaveConfigSO wave in waveConfigs)
+        do
         {
-            currentWave = wave;
-            for(int i = 0; i< currentWave.GetEnemyCount(); i++)
+            foreach(WaveConfigSO wave in waveConfigs)
             {
-                Instantiate(currentWave.GetEnemyPrefab(i),
-                        currentWave.GetStartingtWaypoint().position,
-                        Quaternion.identity,    // this means no rotation
-                        transform);   // put inside the EnemySpawner when instantiating (transform of the parent)
-                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());  // exit coroutine and come back in X seconds
+                currentWave = wave;
+                for(int i = 0; i< currentWave.GetEnemyCount(); i++)
+                {
+                    Instantiate(currentWave.GetEnemyPrefab(i),
+                            currentWave.GetStartingtWaypoint().position,
+                            Quaternion.identity,    // this means no rotation
+                            transform);   // put inside the EnemySpawner when instantiating (transform of the parent)
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());  // exit coroutine and come back in X seconds
+                }
+                yield return new WaitForSeconds(timeBetweenWaves);
             }
-            yield return new WaitForSeconds(timeBetweenWaves);
-        }
-        
+        } while (isLooping);
     }
 }
